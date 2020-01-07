6ec3c10154b5db0006ebe85e2ad3e856d37235e8
Jay Lo <jaylo.eed03@g2.nctu.edu.tw>
2020-01-07 17:58:08 +0800

    RANSAC

--- disparity.py
+++ disparity.py
@@ -6,14 +6,17 @@ import argparse
 DEBUG = False
 
 
-def disp_range(left_img, right_img):
+def max_disp(left_img, right_img):
     """
-    return (min, max) of the disparity
+    get the max disparity
     """
 
+    # Max initial matches
+    MAX_INI_MATCH = np.inf
+
     # ORB detector and brute-force matcher
-    detector = cv2.ORB_create(nfeatures=100000)
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    detector = cv2.ORB_create(nfeatures=100000, edgeThreshold=5)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     # Find the keypoints and descriptors of both images
     keypoint_left, descriptor_left = detector.detectAndCompute(
@@ -23,110 +26,73 @@ def disp_range(left_img, right_img):
         image=right_img, mask=None
     )
 
-    # Find the best 2 matches in both directions
-    match_lr = matcher.knnMatch(
-        queryDescriptors=descriptor_left, trainDescriptors=descriptor_right, k=2
+    # Find the best matches
+    matches = matcher.match(
+        queryDescriptors=descriptor_left, trainDescriptors=descriptor_right
     )
-    match_rl = matcher.knnMatch(
-        queryDescriptors=descriptor_right, trainDescriptors=descriptor_left, k=2
-    )
+    matches = sorted(matches, key=lambda x: x.distance)
 
     if DEBUG:
         print(f"Initial matches#")
-        print(f"match_lr: {len(match_lr)}\t match_rl: {len(match_rl)}")
+        print(f"match_lr: {len(matches)}")
 
-    # ------- Match Refinement -----
-    # 1. Ratio test
-    ratio_thresh = 0.9
-    ratio_match_lr = []
-    ratio_match_rl = []
-
-    for m, n in match_lr:
-        if m.distance < ratio_thresh * n.distance:
-            ratio_match_lr.append(m)
-
-    for m, n in match_rl:
-        if m.distance < ratio_thresh * n.distance:
-            ratio_match_rl.append(m)
-
-    if DEBUG:
-        print(f"After ratio test:")
-        print(f"match_lr: {len(ratio_match_lr)}\t match_rl: {len(ratio_match_rl)}")
-        ratio_img = cv2.drawMatches(
-            left_img,
-            keypoint_left,
-            right_img,
-            keypoint_right,
-            ratio_match_lr,
-            None,
-            flags=2,
-        )
-        cv2.imwrite("./debug/ratio_test.png", ratio_img)
-
-    # 2. Symmetry test
-    sym_match = []
-    # match left -> match right
-    for m_lr in ratio_match_lr:
-        # match right -> match left
-        for m_rl in ratio_match_rl:
-            if m_lr.queryIdx == m_rl.trainIdx and m_lr.trainIdx == m_rl.queryIdx:
-                sym_match.append(m_lr)
-                break  # Next match left -> match right
-
-    if DEBUG:
-        print(f"After symmetry test:")
-        print(f"match: {len(sym_match)}")
-        symm_img = cv2.drawMatches(
-            left_img, keypoint_left, right_img, keypoint_right, sym_match, None, flags=2
-        )
-        cv2.imwrite("./debug/sym_test.png", symm_img)
-
-    # 3. RANSAC
-    left_pts = np.float32([keypoint_left[m.queryIdx].pt for m in sym_match]).reshape(
+    # Match refinement with RANSAC
+    left_pts = np.float32([keypoint_left[m.queryIdx].pt for m in matches]).reshape(
         -1, 1, 2
     )
-    right_pts = np.float32([keypoint_right[m.trainIdx].pt for m in sym_match]).reshape(
+    right_pts = np.float32([keypoint_right[m.trainIdx].pt for m in matches]).reshape(
         -1, 1, 2
     )
     homography, mask = cv2.findHomography(left_pts, right_pts, cv2.RANSAC, 5.0)
     matchesMask = mask.ravel().tolist()
 
+    ransac_matches = []
+    for (m, keep) in zip(matches, matchesMask):
+        if keep:
+            ransac_matches.append(m)
+
     if DEBUG:
         print(f"After RANSAC:")
-        print(f"match: {len(homography)}")
-        draw_params = dict(matchesMask=matchesMask, flags=2)
+        print(f"match: {len(ransac_matches)}")
         ransac_img = cv2.drawMatches(
             left_img,
             keypoint_left,
             right_img,
             keypoint_right,
-            sym_match,
+            ransac_matches,
             None,
-            **draw_params,
+            flags=2,
         )
         cv2.imwrite("./debug/RANSAC.png", ransac_img)
 
-    disp_min = 0
+    # Get the range of disparity
+    disp_min = np.inf
     disp_max = 0
-    return disp_min, disp_max
+
+    for m in ransac_matches:
+        # Keypoints coordinates
+        pt_left = np.float32(keypoint_left[m.queryIdx].pt)
+        pt_right = np.float32(keypoint_right[m.trainIdx].pt)
+
+        # Compute the distance
+        dist = int(abs(pt_left - pt_right)[0])
+
+        if disp_max < dist:
+            disp_max = dist
+
+    # Ceiling the number
+    disp_max += 1
+
+    return disp_max
 
 
 def main():
     parser = argparse.ArgumentParser(description="Disparity Range")
     parser.add_argument(
-        "--input-left",
-        default="./data/Synthetic/TL0.png",
-        type=str,
-        help="input left image",
+        "--input-left", default="data/Real/TL6.bmp", type=str, help="input left image"
     )
     parser.add_argument(
-        "--input-right",
-        default="./data/Synthetic/TR0.png",
-        type=str,
-        help="input right image",
-    )
-    parser.add_argument(
-        "--output", default="./ORB.png", type=str, help="left disparity map"
+        "--input-right", default="data/Real/TR6.bmp", type=str, help="input right image"
     )
     args = parser.parse_args()
 
@@ -134,8 +100,9 @@ def main():
     img_left = cv2.imread(args.input_left)
     img_right = cv2.imread(args.input_right)
     tic = time.time()
-    disp = disp_range(img_left, img_right)
+    disp = max_disp(img_left, img_right)
     toc = time.time()
+    print(f"max disparity: {disp}")
     print("Elapsed time: %f sec." % (toc - tic))
 
 
